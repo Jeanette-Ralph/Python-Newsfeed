f4b4d671efdae45b6caf7ff95188c67164541030
Jeanette Ralph <jean.ralph@hotmail.com>
2022-11-05 16:41:09 -0700

    fixing bugs

--- app/models/Vote.py
+++ app/models/Vote.py
@@ -1,14 +1,9 @@
-from datetime import datetime
 from app.db import Base
-from .Vote import Vote
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
-from sqlalchemy.orm import relationship, column_property
+from sqlalchemy import Column, Integer, ForeignKey
 
 class Vote(Base):
     __tablename__ = 'votes'
-    id = Column(Integer, prrimary_key = True)
+    id = Column(Integer, primary_key = True)
     user_id = Column(Integer, ForeignKey('users.id'))
-    post_id = Column(Integer, ForeignKey('users.id'))
-    vote_count = column_property(
-  select([func.count(Vote.id)]).where(Vote.post_id == id)
-)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    
